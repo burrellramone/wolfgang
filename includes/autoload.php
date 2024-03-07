6a2138f068b74fd34d06cb339b73807120fda769
Ramone Burrell <burrellramone@gmail.com>
2024-03-06 22:10:15 -0500

    Updated spl_autoload_register

--- includes/autoload.php
+++ includes/autoload.php
@@ -1,59 +1,24 @@
 <?php
 set_include_path( get_include_path() . PATH_SEPARATOR . WOLFGANG_DIRECTORY . "vendor/" );
-spl_autoload_extensions( '.class.php,.interface.php,.trait.php' );
+spl_autoload_extensions( '.php,.class.php,.interface.php,.trait.php' );
 
-// SPL Autoload for Non-Test Classes/Traits/Interfaces
-spl_autoload_register( function ( $class ) {
-	if ( preg_match( "/(Wolfgang)/", $class ) ) {
-		
-		$class = str_replace( "\\", "/", $class );
-		$path = WOLFGANG_CLASSES_DIR;
-		$class_parts = explode( "/", $class );
-		
-		array_shift( $class_parts );
-		
-		foreach ( $class_parts as $key => $part ) {
-			if ( $key != 0 ) {
-				$path .= '/';
-			}
-			$path .= $part;
-		}
-		
-		$file = $path . '.class.php';
-		if ( ! file_exists( $file ) ) {
-			$file = $path . '.interface.php';
-			if ( ! file_exists( $file ) ) {
-				$file = $path . '.trait.php';
-				if ( ! file_exists( $file ) ) {
-					return false;
-				}
-			}
-		}
-		include_once ($file);
-	}
-} );
+$composer_classloader = include (WOLFGANG_DIRECTORY . "/vendor/autoload.php");;
+$composer_classmap = $composer_classloader->getClassMap();
 
-// SPL Autoload for Test Classes/Traits/Interfaces
-spl_autoload_register( function ( $class ) {
-	$class = str_replace( "\\", "/", $class );
-	$path = WOLFGANG_TESTS_CLASSES_DIRECTORY;
-	$class_parts = explode( "/", $class );
-	
-	array_shift( $class_parts );
-	array_shift( $class_parts );
-	
-	foreach ( $class_parts as $key => $part ) {
-		if ( $key != 0 ) {
-			$path .= '/';
-		}
-		$path .= $part;
-	}
-	
-	$file = $path . '.class.php';
-	
-	if ( file_exists( $file ) ) {
-		include_once ($file);
-	}
-	
-	return false;
+//new classmap
+$class_map = array();
+
+foreach($composer_classmap as $class => $class_path){
+	$class_map [strtolower($class)] = $class_path;
+}
+
+spl_autoload_register( function ( $class ) use ($class_map){
+
+	$class = strtolower($class);
+
+	if (!isset($class_map[$class])){
+        return false;
+    }
+
+	include_once($class_map[$class]);
 } );
